0d3b0eb56174f0809d2d157c7c1bea7e7fb82d32
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-10-13 19:05:38 -0400

    Clean up main.py

--- main.py
+++ main.py
@@ -1,108 +1,108 @@
-import codecs
-import fastapi
+import base64
+
 from deta import Base, Drive
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse, FileResponse, Response
 from fastapi.responses import Response
+from fastapi.templating import Jinja2Templates
 
+from models import Image
 
+app = FastAPI()
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
+pages = Jinja2Templates(directory="static")
 cdn = Base("images")
 images = Drive("images")
 
 
-class ContentResponse(Response):
+# class ContentResponse(Response):
+#     def __init__(self, path: str, **kwargs):
+#         with open(path, "rb") as f:
+#             content = f.read()
+#             super().__init__(content=content, **kwargs)
+
+
+class NoCacheFileResponse(FileResponse):
     def __init__(self, path: str, **kwargs):
-        with open(path, "rb") as f:
-            content = f.read()
-            super().__init__(content=content, **kwargs)
+        super().__init__(path, **kwargs)
+        self.headers["Cache-Control"] = "no-cache"
 
 
-app = fastapi.FastAPI()
-pages = Jinja2Templates(directory="static")
-app.add_middleware(CORSMiddleware, allow_origins=["*"])
+@app.get("/", response_class=HTMLResponse)
+async def dashboard(request: Request):
+    items = await image_all()
+    return pages.TemplateResponse("dashboard.html", {"request": request, "items": items["data"]})
 
 
-@app.get("/")
-async def dashboard(request: fastapi.Request):
-    res = cdn.fetch()
-    all_items = res.items
-
-    while res.last:
-        res = cdn.fetch(last=res.last)
-        all_items += res.items
-
-    return pages.TemplateResponse(
-        "dashboard.html", {"request": request, "items": all_items}
-    )
-
-
-# to deliver static files withouth caching
+# to deliver static files without caching
+# (Done by jnsougata... smh -- LemonPi314)
 @app.get("/assets/{name}")
-async def file(name: str):
-    return ContentResponse(f"./assets/{name}", media_type="application/octet-stream")
+async def assets(name: str):
+    return NoCacheFileResponse(f"./assets/{name}")
 
 
 @app.get("/scripts/{name}")
-async def file(name: str):
-    return ContentResponse(f"./scripts/{name}", media_type="text/javascript")
+async def scripts(name: str):
+    return NoCacheFileResponse(f"./scripts/{name}")
 
 
 @app.get("/styles/{name}")
-async def file(name: str):
-    return ContentResponse(f"./styles/{name}", media_type="text/css")
+async def styles(name: str):
+    return NoCacheFileResponse(f"./styles/{name}")
 
 
-@app.get("/image")
-async def image_upload_page(request: fastapi.Request):
+@app.get("/image", response_class=HTMLResponse)
+async def image_upload_page(request: Request):
     return pages.TemplateResponse("upload.html", {"request": request})
 
 
 @app.get("/all")
-def image_all():
+async def image_all():
     res = cdn.fetch()
-    all_items = res.items
-
+    items = res.items
     while res.last:
         res = cdn.fetch(last=res.last)
-        all_items += res.items
+        items += res.items
 
-    return {"data": all_items}
+    # Better to just return `items`. -- LemonPi314
+    return {"data": items}
 
 
 @app.patch("/update")
-def image_update(id: str, embed_title: str, embed_colour_hex: str):
-    cdn.update({"embed": [{"title": embed_title, "colour": embed_colour_hex}]}, id)
+async def image_update(id: str, embed_title: str, embed_colour_hex: str):
+    cdn.update({"embed": [{"title": embed_title, "colour": embed_colour_hex}]}, key=id)
     return {"id": id}
 
 
 @app.post("/upload")
-async def upload_image(request: fastapi.Request):
-    data = await request.json()
-    filename = data["filename"]
-    extension = filename.split(".")[-1]
-    image_data = data["content"].split(",")[1].encode("utf-8")
-    name = cdn.put(
+async def image_upload(request: Request, image: Image):
+    extension = image.filename.split(".")[-1]
+    image_data = image.content.split(",")[1].encode("utf-8")
+    item = cdn.put(
         {
             "ext": extension,
+            # Why non-US spelling? Might come as a suprise to users of the API.
+            # Just the way it is, not trying to say US is superior. -- LemonPi314
             "embed": [{"title": None, "colour": None}],
-        }
+        },
     )
-    images.put(f"{name['key']}.{extension}", codecs.decode(image_data, "base64"))
-    uri = f"{request.url.scheme}://{request.url.hostname}/cdn/{name['key']}.{extension}"
-    return {"image": uri, "id": name["key"]}
+    id = item["key"]
+    images.put(f"{id}.{extension}", base64.b64decode(image_data))
+    url = f"{request.url.scheme}://{request.url.hostname}/cdn/{id}.{extension}"
+    return {"image": url, "id": id}
 
 
 @app.delete("/delete")
-def delete_image(id: str):
+async def image_delete(id: str):
     data = cdn.get(id)
     images.delete(f"{data['key']}.{data['ext']}")
     cdn.delete(id)
     return {"id": id}
 
 
-@app.get("/info/{id}")
-def image_info(request: fastapi.Request, id: str):
+@app.get("/info/{id}", response_class=HTMLResponse)
+async def image_info(request: Request, id: str):
     info = cdn.get(id)
     return pages.TemplateResponse(
         "info.html",
@@ -111,17 +111,18 @@ def image_info(request: fastapi.Request, id: str):
 
 
 @app.get("/cdn/{image}")
-def image_cdn(image: str):
+async def image_cdn(image: str):
     img = images.get(image)
-    return fastapi.responses.StreamingResponse(
-        img.iter_chunks(), media_type=f"image/{image.split('.')[1]}"
+    return Response(
+        img.read(),
+        media_type=f"image/{image.split('.')[1]}",
     )
 
 
 @app.get("/embed/{image}")
-def image_cdn_embed(request: fastapi.Request, image: str):
+async def image_cdn_embed(request: Request, image: str):
     embed = cdn.get(image.split(".")[0])
-    return fastapi.responses.HTMLResponse(
+    return HTMLResponse(
         f"""
         <meta name="twitter:card" content="summary_large_image">
         <meta property="og:title" content="{embed["embed"][0]["title"]}"/>
