5027d90830d3d23811ffed4c1a4518d8b1fb5b72
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-10-13 23:54:37 -0400

    Add alert to share button

--- static/scripts/dashboard.js
+++ static/scripts/dashboard.js
@@ -3,4 +3,5 @@ let shareButton = document.getElementById("share");
 shareButton.addEventListener("click", () => {
   let imginfo = document.getElementById("imginfo");
   navigator.clipboard.writeText(`${window.location.origin}/cdn/${imginfo.innerHTML}`);
+  alert("Link copied to clipboard.");
 });
