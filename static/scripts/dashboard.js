468222893458d4099398d682c5c3d2115f72d500
jnsougata <jnsougata@gmail.com>
2022-10-17 04:19:40 +0530

    might fix

--- static/scripts/dashboard.js
+++ static/scripts/dashboard.js
@@ -3,15 +3,16 @@ let shareButtons = document.getElementsByClassName("share-button");
 for (let button of shareButtons) {
   button.addEventListener("click", () => {
     let url = `${window.location.origin}/cdn/${button.id}`
-
     fetch(`/data/${button.id}`)
     .then((res) => res.json())
     .then((data) => {
       if (data.visibility == false) {
-        alert("This image is private. You can't share it.");
+        alert("Image is private. You can't share it.");
       } else {
-        navigator.clipboard.writeText(url);
-        alert("URL xopied to clipboard!");
+        navigator.clipboard.writeText(url)
+        .then(() => {
+          alert("Image is public. Link copied to clipboard.");
+        });
       }
     })
   });
