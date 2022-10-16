1d9fa4a43b16a6ff99386ef6f8cc6ca65c7ac27b
jnsougata <jnsougata@gmail.com>
2022-10-17 03:36:07 +0530

    fixes

--- static/scripts/dashboard.js
+++ static/scripts/dashboard.js
@@ -2,9 +2,17 @@ let shareButtons = document.getElementsByClassName("share-button");
 
 for (let button of shareButtons) {
   button.addEventListener("click", () => {
-    let imginfo = document.getElementById("imginfo");
-    navigator.clipboard.writeText(`${window.location.origin}/cdn/${imginfo.innerHTML}`);
-    alert("Link copied to clipboard.");
-    // Fix copy to clipboard only working once issue and add error alert when the image returns an error (404) saying that the image isn't visable (Public)
+    let url = `${window.location.origin}/cdn/${button.id}`
+
+    fetch(`/data/${button.id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.visibility == false) {
+        alert("This image is private. You can't share it.");
+      } else {
+        navigator.clipboard.writeText(url);
+        alert("URL xopied to clipboard!");
+      }
+    })
   });
 }
